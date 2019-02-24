49843cf3f5db5a6628ab1ea28d3e9261ff912ee7
RANJIT JHA <ranjit.jha@in.ibm.com>
2019-02-24 18:28:56 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express= require('express')
+
+const hbs= require('hbs')
+
+const fs= require('fs');
+
+var app=express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+hbs.registerHelper('currentYear',()=>{
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt',(text)=>{
+    return text.toUpperCase();
+});
+
+app.set('view engine','hbs');
+
+app.use((req,res,next)=>{
+    var log=`${new Date().toString()}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log',log, (error)=>{
+        if(error){
+            console.log('Unable to write to file');
+        }
+    });
+    next();
+});
+
+
+app.use((req,res,next)=>{
+    res.render('maintainance')
+
+});
+
+app.use(express.static(__dirname+'/public'));
+
+app.get('/',(req,res)=>{
+    res.render('home.hbs',{
+        pageHeader:'Home Page',
+        welcomeMessage:'Welcome to new brand site'
+    })
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs',{
+        pageHeader:'About Page'
+    });
+})
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage:"Bad Request"
+    });
+})
+
+app.listen(3000);
